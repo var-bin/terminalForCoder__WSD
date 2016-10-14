5aae36693fce666c3a65c1556f331069a7b28fb2
var-bin <var-bin@users.noreply.github.com>
2016-10-14 19:50:14 +0300

    add some debugging info

--- screen/chat-example/index.js
+++ screen/chat-example/index.js
@@ -7,11 +7,20 @@ app.get('/', function(req, res){
 });
 
 io.on('connection', function(socket){
+
+  console.log("user: " + socket.id + " is connect");
+
   socket.on('chat message', function(msg){
+    console.log("user: " + socket.id + " has sent you message: " + msg);
     io.emit('chat message', msg);
   });
+
+});
+
+io.on("disconnect", function(socket) {
+  console.log("user: " + socket.id + " is disconnect");
 });
 
 http.listen(3000, function(){
-  console.log('listening on *:3000');
+  console.log('listening on localhost:3000');
 });
