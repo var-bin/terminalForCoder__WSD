b4f3ac65a5d235f676eab797b270c814b0c6eace
var-bin <var-bin@users.noreply.github.com>
2016-11-05 21:55:04 +0200

    add clear_dist.sh

---
+++ bash/scripting/clear_dist.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# clear_dist.sh - clear dist for current theme
+
+
+clearDist() {
+  theme=$1
+  NAME="_dist"
+
+  path_to_assets="/d/var-bin/terminalForCoder__WSD/bash/core/assets"
+
+  # if $1 == "" clear all _dist in each theme
+  if [[ -z $theme ]]
+  then
+    theme="assets"
+    path_to_dist="$path_to_assets/"
+  else
+    path_to_dist="$path_to_assets/$theme"
+  fi
+
+  if [[ -n $(find $path_to_dist -type d -name $NAME) ]]
+  then
+    # do clear stuff
+    find $path_to_dist -type d -name $NAME | xargs -l rm -rfv
+    echo "Dist of $theme have already deleted: $path_to_dist"
+  else
+    echo "Can not find $NAME in $theme"
+  fi
+}
+
+clearDist $@
