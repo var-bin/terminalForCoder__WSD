5f6cf75e955a96e4f2165b110d0ac018bf19e3f4
var-bin <var-bin@users.noreply.github.com>
2016-11-16 21:38:04 +0200

    add script for cloning vendors repositories to /c/Users/var/terminalForCoder__WSD/bash/core/vendors/

---
+++ bash/clone_vendors.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+# clone vendors repositories to ./core/vendors
+REPO_NAME="terminalForCoder__WSD"
+PATH_TO_CORE="$HOME/$REPO_NAME/bash/core/vendors/"
+
+# array with repositories
+repositories=( "https://github.com/larscmagnusson/CSS3MultiColumn.git" "https://github.com/tc39/test262.git" "https://github.com/postcss/postcss" "https://github.com/webpack/webpack" "https://github.com/var-bin/spriteFactory.git" "https://github.com/var-bin/backbone-training.git" "https://github.com/var-bin/flex-grid-framework.git" "https://github.com/var-bin/BrandButtons.git" "https://github.com/var-bin/less-easings.git" )
+
+i=0 # start el
+repositories_count=${#repositories[@]} # array size
+
+cloneVendors() {
+  while [ $i -lt $repositories_count ]
+  do
+    # Get vendor name
+    vendor=$(echo ${repositories[$i]} | sed "s/https\:\/\/github\.com\/*//g" | sed "s/\/.*//g")
+    vendor_repo_name=$(echo ${repositories[$i]} | sed "s/https\:\/\/github\.com\/.*\///g" | sed "s/\.git//g")
+
+    if [[ -d "$PATH_TO_CORE/$vendor/$vendor_repo_name" ]]
+    then
+      echo "Directory $PATH_TO_CORE/$vendor is exist"
+    else
+      echo "Create directory: $PATH_TO_CORE/$vendor/$vendor_repo_name"
+      echo "Repository: ${repositories[$i]} is clonning"
+
+      mkdir "$PATH_TO_CORE/$vendor/$vendor_repo_name" && cd "$PATH_TO_CORE/$vendor/$vendor_repo_name" && git clone ${repositories[$i]} && cd -
+    fi
+    i=$((i + 1))
+  done
+}
+
+cloneVendors $@
