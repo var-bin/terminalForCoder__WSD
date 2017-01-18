dc042820683b4c540b9b0b84ef5594d8ec0e539d
var-bin <var-bin@users.noreply.github.com>
2017-01-18 18:44:43 +0200

    Edit echo messages

--- bash/scripting/if_elif_else.sh
+++ bash/scripting/if_elif_else.sh
@@ -2,11 +2,11 @@
 
 if [[ -f "$1" ]]
 then
-  echo "Removing file"
+  echo "Removing file ${1}"
   rm "$1"
 elif [[ -d "$1" ]]
 then
-  echo "Removing dir"
+  echo "Removing dir ${1}"
   rm -r "$1"
 else
   echo "Can't remove ${1}"
