c45a5467e8ca4a75b2d3d274f455d0313b31b8e7
var-bin <var-bin@users.noreply.github.com>
2017-01-18 16:25:33 +0200

    Code style fixed.

--- bash/scripting/clean_dist.sh
+++ bash/scripting/clean_dist.sh
@@ -2,30 +2,29 @@
 
 # clean_dist.sh - clean dist for current theme
 
-
 cleanDist() {
   theme="$1"
   DIST_NAME="_dist"
   REPO_NAME="terminalForCoder__WSD"
 
-  path_to_assets="$HOME/$REPO_NAME/bash/core/assets"
+  path_to_assets="${HOME}/${REPO_NAME}/bash/core/assets"
 
   # if $1 == "" clean all _dist in each theme
   if [[ -z "$theme" ]]
   then
     theme="assets"
-    path_to_dist="$path_to_assets/"
+    path_to_dist="$path_to_assets"
   else
-    path_to_dist="$path_to_assets/$theme"
+    path_to_dist="${path_to_assets}/${theme}"
   fi
 
-  if [[ -n $(find $path_to_dist -type d -name $DIST_NAME) ]]
+  if [[ -n $(find "$path_to_dist" -type d -name "$DIST_NAME") ]]
   then
     # do clear stuff
     find "$path_to_dist" -type d -name "$DIST_NAME" | xargs -l rm -rfv
-    echo "Dist of $theme have already deleted: $path_to_dist"
+    echo "Dist of ${theme} have already deleted: ${path_to_dist}"
   else
-    echo "Can not find $DIST_NAME in $theme"
+    echo "Can not find ${DIST_NAME} in ${theme}"
   fi
 }
 
