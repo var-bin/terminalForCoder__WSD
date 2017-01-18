c8d957ed73561e6fbb4dc5f64dd41b304b11e138
var-bin <var-bin@users.noreply.github.com>
2017-01-18 16:26:48 +0200

    Code style fixed.

--- bash/scripting/fast_diff.sh
+++ bash/scripting/fast_diff.sh
@@ -2,16 +2,16 @@
 
 # create diff for current branch and puts it in the file
 
-PATH_TO_DIFF_DIR="$HOME/diff/"
+PATH_TO_DIFF_DIR="${HOME}/diff/"
 FILE_PREFIX="diff-"
 FILE_EXTENTION=".diff"
 
-branch=$1
+branch="$1"
 
-if [[ -z $branch ]]
+if [[ -z "$branch" ]]
 then
   echo "Enter the branch name"
 else
-  path="$PATH_TO_DIFF_DIR$FILE_PREFIX$branch$FILE_EXTENTION"
+  path="${PATH_TO_DIFF_DIR}${FILE_PREFIX}${branch}${FILE_EXTENTION}"
   git diff origin/master origin/"$branch" > "$path"
 fi
