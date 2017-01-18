6d7a2828107892ad2ac4bbecaca93262a95e8645
var-bin <var-bin@users.noreply.github.com>
2017-01-18 16:27:39 +0200

    Code style fixed.

--- bash/scripting/fast_diff_v2.sh
+++ bash/scripting/fast_diff_v2.sh
@@ -12,14 +12,14 @@ branch=$(git rev-parse --abbrev-ref HEAD)
 # Get the first line of git remote output and cut a path to repository
 repository=$(git remote -v | head -n1 | sed "s/^origin.*\/\(.*\)\.git\s(.*)/\1/")
 
-path_to_diff="$HOME/<path_to_diff_directory>$branch-$repository.diff"
+path_to_diff="${HOME}/<path_to_diff_directory>${branch}-${repository}.diff"
 
 diff_live () {
-  git diff "origin/live..master/$branch" > "$path_to_diff"
+  git diff "origin/live..master/${branch}" > "$path_to_diff"
 }
 
 attach_diff () {
-  curl -D- -u "<ipa_username>":"<ipa_password>" -X POST -H "X-Atlassian-Token: no-check" -F "file=@$path_to_diff;type=text/x-diff" "https://jira.<project_name>.com/rest/api/2/issue/$issue_id/attachments"
+  curl -D- -u "<ipa_username>":"<ipa_password>" -X POST -H "X-Atlassian-Token: no-check" -F "file=@${path_to_diff};type=text/x-diff" "https://jira.<project_name>.com/rest/api/2/issue/${issue_id}/attachments"
 }
 
 diff_live && attach_diff
