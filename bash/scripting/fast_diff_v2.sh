65337ff38d9520d79ef00a51554a753f80d7d670
var-bin <var-bin@users.noreply.github.com>
2017-02-05 23:08:59 +0200

    Some refactoring

--- bash/scripting/fast_diff_v2.sh
+++ bash/scripting/fast_diff_v2.sh
@@ -3,8 +3,8 @@
 PATH_TO_DIFF_DIR="${HOME}/diff/"
 FILE_EXTENTION=".diff"
 
-USER_NAME="<ipa_user_name>"
-USER_PASSWORD="<ipa_user_password>"
+USER_NAME="<user_name>"
+USER_PASSWORD="<user_password>"
 PROJECT_NAME="<project_name>"
 
 # if "$1" is empty
@@ -24,16 +24,17 @@ file_name="${branch}-${repository}${FILE_EXTENTION}"
 # path to diff directory with <filename>.diff
 path_to_diff="${PATH_TO_DIFF_DIR}${file_name}"
 
-diff_master() {
+diffMaster() {
   git diff "origin/master origin/${branch}" > "$path_to_diff"
 }
 
-attach_diff() {
+attachDiff() {
   curl -D -u "${USER_NAME}":"${USER_PASSWORD}" -X POST -H "X-Atlassian-Token: no-check" -F "file=@${path_to_diff};type=text/x-diff" "https://jira.${PROJECT_NAME}.com/rest/api/2/issue/${issue_id}/attachments"
 }
 
-diff_master && attach_diff
+diffMaster && attachDiff
 
 # Usage: cd <repo_name> && fast_diff_v2.sh <issue_id>
 # <issue_id> should include your company prefix (ABC, XYZ, XX, etc.)
-# At instance, "./fast_diff_v2.sh XYZ-135" will try to attach diff to https://jira.<project_name>.com/browse/XYZ-135
+# At instance, "./fast_diff_v2.sh XYZ-135" will try to attach diff to
+# https://jira.<project_name>.com/browse/XYZ-135
